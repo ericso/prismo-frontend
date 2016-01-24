ffc48b310fb25a380d1837e8cdc4ece94f8be563
Eric So <me@eric.so>
2016-01-24 00:02:38 -0800

    Removed deprecated version directive.

--- app/components/version/directives.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular
-  .module('prismo.appinfo.directives', [])
-  .directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
